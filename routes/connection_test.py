955f8e50e998dfe8fba00f913d45fb7a0f79931c
Matthew Tran <matthewtran0122@gmail.com>
2024-02-19 23:52:55 -0800

    working sample connection code
    TODO: make the .env file connect from outside the repo URGENT

---
+++ routes/connection_test.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+
+env_file = os.environ.get("dbLogin_env")
+load_dotenv(env_file)
+
+import MySQLdb
+
+host=os.getenv("DB_HOST")
+user=os.getenv("DB_USERNAME")
+passwd=os.getenv("DB_PASSWORD")
+db=os.getenv("DB_NAME")
+print("Attempting connection")
+print(host)
+print(user)
+
+connection = MySQLdb.connect(
+host,
+user,
+passwd,
+db,
+autocommit=True,
+
+)
+print("connection success")
+connection.close()
+print("connection closed")
+
+
+
+
+
