2b6b0892aa3521882b3332b3a11d05968cc72363
Jason <hieu.hc04@gmail.com>
2024-03-11 12:04:13 -0700

    Changed Rounting

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {userRef} from 'react';
+import Login from './pages/Login';
+import ReportPurchases from './pages/ReportPurchases';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes> 
+        <Route path="/" element={<Login /> } />
+        <Route path="/ReportPurchases" element={<ReportPurchases /> } />
+
+      </Routes>
+    </Router>
   );
 }
 
