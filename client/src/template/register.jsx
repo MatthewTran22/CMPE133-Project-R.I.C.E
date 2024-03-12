948154022964df2e0e3b9384a0965bfc0516f851
Matthew Tran <matthewtran0122@gmail.com>
2024-03-11 20:22:09 -0700

    register setup
    not done yet

--- client/src/template/register.jsx
+++ client/src/template/register.jsx
@@ -1,6 +1,24 @@
 import React, {useRef, useState, useEffect } from 'react';
 
 const Register = () => {
+  const useRef = useRef();
+  const errRef = useRef();
+
+  const[email, setEmail] = useState('');
+  const[validEmail, setValidEmail] = useState(false);
+  const[emailFocus, setEmailFocus] = useState(false);
+
+  const[pwd, setPwd] = useState('');
+  const[validPwd, setValidPwd] = useState(false);
+  const[pwdFocus, setPwdFocus] = useState(false);
+
+  const[matchPwd, setMatchPwd] = useState('');
+  const[validMatch, setValidMatch] = useState(false);
+  const[matchFocus, setMatchFocus] = useState(false);
+
+  const[errMsg, setErrMsg] = useState('');
+  const[success, setSuccess] = useState(false);
+  
   return (
     <div>
       <h1>
