28ec8794dad07cb987fe3aa4a8a93ebe597d0e99
Daniel Lee <daniel.lee02@sjsu.edu>
2024-03-17 15:16:10 -0700

    Added Home Page
    Home page is running properly.
    Will make it look nice now.

--- client/src/template/Home.jsx
+++ client/src/template/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ricePattyImg from './images/ricepatty.png';
+import riceLogoImg from './images/ricelogo.png';
+import piggyBankImg from './images/piggybank.png';
 import '../styles.css';
 const Front = () => {
   const Navigate = useNavigate();
@@ -17,47 +20,44 @@ const Front = () => {
   return (
     <div>
       <h1>
-      <div className="box">
-        <div className="image-container">
-          <img className="bgpng" src="ricepatty.png" alt="Box 1 Image" />
-          <div className="text-overlay">
-            <img src="ricelogo.png" alt="Box 1 Image" />
-            <b>a lightweight tool to help you budget and save</b>
+        <div className="box">
+          <div className="image-container">
+            <img className="bgpng" src={ricePattyImg} alt="Box 1 Image" />
+            <div className="text-overlay">
+              <img src={riceLogoImg} alt="Box 1 Image" />
+              <b>a lightweight tool to help you budget and save</b>
+              <button onClick={goToLogin}>Go to Login</button>
+              <button onClick={goToRegister}>Go to Register</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="box">
-        <b>Meet the team</b>
-      </div>
+        <div className="box">
+          <b>Meet the team</b>
+        </div>
 
-      <div className="box">
-        <div className="image-container">
-          <img className="bgpng" src="piggybank.png" alt="Box 1 Image" />
-          <div className="text-overlay">
-            <b>penny saved is a rice earned wwww</b>
+        <div className="box">
+          <div className="image-container">
+            <img className="bgpng" src={piggyBankImg} alt="Box 1 Image" />
+            <div className="text-overlay">
+              <b>penny saved is a rice earned wwww</b>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="box">
-        <b>banana</b>
-      </div>
+        <div className="box">
+          <b>banana</b>
+        </div>
 
-      <div className="footer">
-        Copyright © 2024 RICE
-      </div>
+        <div className="footer">
+          Copyright © 2024 RICE
+        </div>
 
-      {/* Scroll to top button */}
-      <div className="scroll-to-top">
-        <div className="circle"></div>
-      </div>
+        {/* Scroll to top button */}
+        <div className="scroll-to-top">
+          <div className="circle"></div>
+        </div>
       </h1>
-      <br />
-      <h1>Welcome to Home Page</h1>
-      <br />
-      <button onClick={goToLogin}>Go to Login</button>
-      <button onClick={goToRegister}>Go to Register</button>
        
     </div>
   );  
