f217c11b9c10bd5d26e816134f43cb11e67a05ed
Daniel Lee <daniel.lee02@sjsu.edu>
2024-03-17 14:59:56 -0700

    Home Page Updated
    I'm not sure if the home page works since I cant connect to the server yet. Will fix if broken whenever my connection starts working. I left the buttons to the login and register at the bottom.

--- client/src/template/Home.jsx
+++ client/src/template/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles.css';
 const Front = () => {
   const Navigate = useNavigate();
 
@@ -16,7 +17,41 @@ const Front = () => {
   return (
     <div>
       <h1>
-        NOTE FOR DEVELOPER WORKING ON FRONT: paste HTML code here and edit what is needed
+      <div className="box">
+        <div className="image-container">
+          <img className="bgpng" src="ricepatty.png" alt="Box 1 Image" />
+          <div className="text-overlay">
+            <img src="ricelogo.png" alt="Box 1 Image" />
+            <b>a lightweight tool to help you budget and save</b>
+          </div>
+        </div>
+      </div>
+
+      <div className="box">
+        <b>Meet the team</b>
+      </div>
+
+      <div className="box">
+        <div className="image-container">
+          <img className="bgpng" src="piggybank.png" alt="Box 1 Image" />
+          <div className="text-overlay">
+            <b>penny saved is a rice earned wwww</b>
+          </div>
+        </div>
+      </div>
+
+      <div className="box">
+        <b>banana</b>
+      </div>
+
+      <div className="footer">
+        Copyright © 2024 RICE
+      </div>
+
+      {/* Scroll to top button */}
+      <div className="scroll-to-top">
+        <div className="circle"></div>
+      </div>
       </h1>
       <br />
       <h1>Welcome to Home Page</h1>
