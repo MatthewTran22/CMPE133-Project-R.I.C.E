d36fb3a8d5a16b76cf9db868d39e7a4c4c7763dd
Jason <hieu.hc04@gmail.com>
2024-03-19 19:24:36 -0700

    Register page text to white
    Fix the scrolling issue.

--- client/src/template/register.jsx
+++ client/src/template/register.jsx
@@ -1,6 +1,5 @@
-import React, {useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import Redirect from React Router
-
 
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //this checks if the email has at least 2 letters before the email name
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/; //this checks if the password has at least 1 lower case, one upper case, one number and one special character
@@ -10,57 +9,47 @@ const Register = () => {
   const userRef = useRef();
   const errRef = useRef();
 
-  const[email, setEmail] = useState('');
-  const[validEmail, setValidEmail] = useState(false);
-  const[emailFocus, setEmailFocus] = useState(false);
+  const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
 
-  const[pwd, setPwd] = useState('');
-  const[validPwd, setValidPwd] = useState(false);
-  const[pwdFocus, setPwdFocus] = useState(false);
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
 
-  const[matchPwd, setMatchPwd] = useState('');
-  const[validMatch, setValidMatch] = useState(false);
-  const[matchFocus, setMatchFocus] = useState(false);
+  const [matchPwd, setMatchPwd] = useState('');
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
-  const[errMsg, setErrMsg] = useState('');
-  const[success, setSuccess] = useState(false);
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
 
-  
-
-  useEffect (() => {
+  useEffect(() => {
     userRef.current.focus();
-  },
-  [])
+  }, []);
 
-  useEffect (() => {
+  useEffect(() => {
     const result = EMAIL_REGEX.test(email); //calls email regex to check if email format is valid returns boolean
-    console.log(result);
-    console.log(email);
     setValidEmail(result);
-  },
-  [email])
+  }, [email]);
 
-  useEffect (() => {
+  useEffect(() => {
     const result = PWD_REGEX.test(pwd); //calls password regex to check if password is valid returns boolean
-    console.log(result);
-    console.log(pwd);
     setValidPwd(result);
     const match = pwd === matchPwd; //checks if both password entries are correct returns boolean
     setValidMatch(match);
-  },
-  [pwd, matchPwd])
+  }, [pwd, matchPwd]);
 
-  useEffect (() => {
+  useEffect(() => {
     setErrMsg('');
-  },
-  [email,pwd,matchPwd])
+  }, [email, pwd, matchPwd]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     //to prevent JS hack
     const v1 = EMAIL_REGEX.test(email);
     const v2 = PWD_REGEX.test(pwd);
-    if(!v1 || !v2){
+    if (!v1 || !v2) {
       setErrMsg("Invalid Entry");
       return; //will come back to this, this section should be calling the python backend to insert the data into the SQL DB
     }
@@ -84,11 +73,11 @@ const Register = () => {
         setErrMsg('');
         setSuccess(true);
         nav('../login');
-      } 
-      else if(err.includes('23505')){
+      }
+      else if (err.includes('23505')) {
         setErrMsg("Email already Registered");
       }
-      else{
+      else {
         setErrMsg("Something went wrong! Please try again in a few minutes!");
       }
       // Handle success or display any error messages
@@ -99,84 +88,68 @@ const Register = () => {
       // Handle error
     }
   }
-  
+
   return (
-    <section>
-      <h1>
-        Create an Account
-      </h1> <br />
+    <section style={{ color: 'white' }}>
+      <h1>Create an Account</h1> <br />
       <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email"> {/* input field for email */}
-          Email:
-        </label>
-        {/* input specifications */}
-        <input 
-        type="text"
-        id="email"
-        ref={userRef}
-        autoComplete="off"
-        onChange={(e) => setEmail(e.target.value)}
-        required
-        aria-invalid={validEmail ? "false" : "true"}
-        aria-describedby="uidnote"
-        onFocus={() => setEmailFocus(true)}
-        onBlur={() => setEmailFocus(false)}
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          aria-invalid={validEmail ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setEmailFocus(true)}
+          onBlur={() => setEmailFocus(false)}
         />
-        <p id="uidnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}> {/* pop-up for invalid format, currently needs fix */}
+        <p id="uidnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
           At least 2 characters <br />
           Needs to be a proper email address <br />
           TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
         </p>
 
-        <label htmlFor="password"> {/* input field for password */}
-          Password: 
-        </label>
-        {/* input specifications */}
-        <input 
-        type="password"
-        id="password"
-        onChange={(e) => setPwd(e.target.value)}
-        required
-        aria-invalid={validPwd ? "false" : "true"}
-        aria-describedby="pwdnote"
-        onFocus={() => setPwdFocus(true)}
-        onBlur={() => setPwdFocus(false)}
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          required
+          aria-invalid={validPwd ? "false" : "true"}
+          aria-describedby="pwdnote"
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
         />
-        <p id="pwdnote" className={pwdFocus && pwd && !validPwd ? "instructions" : "offscreen"}> {/* pop-up for invalid format, currently needs fix */}
+        <p id="pwdnote" className={pwdFocus && pwd && !validPwd ? "instructions" : "offscreen"}>
           8 to 24 characters <br />
-         Must include one lower case, one upper case, one number, and one special character <br />
-         Allowed special charaters: <span aria-label='exclemation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span> <br />
+          Must include one lower case, one upper case, one number, and one special character <br />
+          Allowed special characters: <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span> <br />
           TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
         </p>
 
-        <label htmlFor="confirm_pwd"> {/* input field for confirm password */}
-          Confirm Password: 
-        </label>
-        {/* input specifications */}
-        <input 
-        type="password"
-        id="confirm_pwd"
-        onChange={(e) => setMatchPwd(e.target.value)}
-        required
-        aria-invalid={validMatch ? "false" : "true"}
-        aria-describedby="confirmnote"
-        onFocus={() => setMatchFocus(true)}
-        onBlur={() => setMatchFocus(false)}
+        <label htmlFor="confirm_pwd">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm_pwd"
+          onChange={(e) => setMatchPwd(e.target.value)}
+          required
+          aria-invalid={validMatch ? "false" : "true"}
+          aria-describedby="confirmnote"
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
         />
-        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}> {/* pop-up for invalid format, currently needs fix */}
+        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
           8 to 24 characters <br />
-          Password does not match<br />
+          Password does not match <br />
           TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
         </p>
 
-        <button disabled={!validEmail || !validPwd || !validMatch ? true: false}>Create an Account</button>
-
-
+        <button disabled={!validEmail || !validPwd || !validMatch ? true : false}>Create an Account</button>
       </form>
-
-      
-     
     </section>
   );
 };
