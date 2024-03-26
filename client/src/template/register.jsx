ef0d23051950433adfec67b3e395628cff61ef04
Matthew Tran <matthewtran0122@gmail.com>
2024-03-26 13:31:34 -0700

    updated register css

--- client/src/template/register.jsx
+++ client/src/template/register.jsx
@@ -75,7 +75,7 @@ const Register = () => {
         nav('../login');
       }
       else if (err.includes('23505')) {
-        setErrMsg("Email already Registered");
+        setErrMsg("ERROR: An account has already been made with this email");
       }
       else {
         setErrMsg("Something went wrong! Please try again in a few minutes!");
@@ -90,7 +90,11 @@ const Register = () => {
   }
 
   return (
-    <section>
+    <section className="flex min-h-full h-screen flex-col justify-center px-6 py-12 lg:px-8">
+     <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
+            <div id="title"></div>
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-20 w-auto drop-shadow-2xl"
@@ -101,19 +105,11 @@ const Register = () => {
           Create an Account
         </h2>
       </div>
-      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-      <div
-        className="mt-10 sm:mx-auto sm:w-full sm:max-w-md border-2 rounded-lg bg-slate-200 p-5 flex flex-col justify-center"
-        style={{ minHeight: "300px", minWidth: "500px" }}
-      >
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md border-2 rounded-lg bg-slate-200 p-5 flex flex-col justify-center" style={{ minHeight: "300px", minWidth: "500px" }}>
         <form onSubmit={handleSubmit}>
+        <p ref={errRef} className={`text-center ${errMsg ? "text-red-600" : "hidden"}`} aria-live="assertive">{errMsg}</p>
           <div className="mb-6">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-stone-800"
-            >
-              Email address
-            </label>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-stone-800">Email address</label>
             <input
               type="text"
               id="email"
@@ -125,73 +121,66 @@ const Register = () => {
               aria-describedby="uidnote"
               onFocus={() => setEmailFocus(true)}
               onBlur={() => setEmailFocus(false)}
+              className="w-full rounded-md shadow-sm"
             />
-            <p id="uidnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
-              At least 2 characters <br />
-              Needs to be a proper email address <br />
-              TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
-            </p>
           </div>
           <div className="mb-6">
             <div className="flex justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-stone-800"
-              >
-                Password
-              </label>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-stone-800">Password</label>
             </div>
-            <div className="mt-1 rounded-md shadow-sm">
-              <input
-                type="password"
-                id="password"
-                onChange={(e) => setPwd(e.target.value)}
-                required
-                aria-invalid={validPwd ? "false" : "true"}
-                aria-describedby="pwdnote"
-                onFocus={() => setPwdFocus(true)}
-                onBlur={() => setPwdFocus(false)}
-              />
-              <p id="pwdnote" className={pwdFocus && pwd && !validPwd ? "instructions" : "offscreen"}>
-                8 to 24 characters <br />
-                Must include one lower case, one upper case, one number, and one special character <br />
-                Allowed special characters: <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span> <br />
-                TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
-              </p>
-            </div>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              required
+              aria-invalid={validPwd ? "false" : "true"}
+              aria-describedby="pwdnote"
+              onFocus={() => setPwdFocus(true)}
+              onBlur={() => setPwdFocus(false)}
+              className="w-full rounded-md shadow-sm"
+            />
           </div>
           <div className="mb-6">
             <div className="flex justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-stone-800"
-              >
-                Confirm Password
-              </label>
+              <label htmlFor="confirm_pwd" className="block text-sm font-medium leading-6 text-stone-800">Confirm Password</label>
             </div>
-            <div className="mt-1 rounded-md shadow-sm">
-              <input
-                type="password"
-                id="confirm_pwd"
-                onChange={(e) => setMatchPwd(e.target.value)}
-                required
-                aria-invalid={validMatch ? "false" : "true"}
-                aria-describedby="confirmnote"
-                onFocus={() => setMatchFocus(true)}
-                onBlur={() => setMatchFocus(false)}
-              />
-              <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                8 to 24 characters <br />
-                Password does not match <br />
-                TODO: MAKE THIS APPEAR AND DISAPPEAR BASED ON THE STATUS OF THE TEXT BOX
-              </p>
-            </div>
+            <input
+              type="password"
+              id="confirm_pwd"
+              onChange={(e) => setMatchPwd(e.target.value)}
+              required
+              aria-invalid={validMatch ? "false" : "true"}
+              aria-describedby="confirmnote"
+              onFocus={() => setMatchFocus(true)}
+              onBlur={() => setMatchFocus(false)}
+              className="w-full rounded-md shadow-sm"
+            />
           </div>
-          <button disabled={!validEmail || !validPwd || !validMatch ? true : false}>Create an Account</button>
+          <div className='mb-6'>
+          <a
+                href="./login"
+                className="text-sm font-medium leading-5 text-blue-600 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+              >
+                Already have an account? Login Here
+            </a> 
+          </div>
+          
+          <div className="mb-6">
+            <span className="block w-full rounded-md shadow-sm">
+              <button
+                disabled={!validEmail || !validPwd || !validMatch ? true : false}
+                type="submit"
+                className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+              >
+                Create an Account
+              </button>
+            </span>
+          </div>
         </form>
       </div>
     </section>
-  );  
+  );
+  
 };
 
 export default Register;
