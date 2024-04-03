99f71becef37a382f25a5187bb5dfc2fca05ca1a
Matthew Tran <matthewtran0122@gmail.com>
2024-04-02 18:02:48 -0700

    started on input-info file

--- client/src/template/input-info.jsx
+++ client/src/template/input-info.jsx
@@ -1,11 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import Redirect from React Router
+import { useNavigate } from 'react-router-dom';
+import useSessionChecker from '../components/SessionCheck';
 
 
-const useUserInfo = () => {
- 
+const InputInfo = () => {
+  const navigate = useNavigate();
+  useSessionChecker();
+  const [income, setIncome] = useState('');
 
-  return userInfo;
+  // Use an effect to redirect to a results page when both income and savings are set
+  useEffect(() => {
+    if (income !== '') {
+      navigate('/results', { state: { income} });
+    }
+  }, [income, navigate]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (income !== '') {
+      navigate('/results', { state: { income} });
+    }
+  };
+
+  return (
+    <div>
+      <h2>Enter your monthly income and savings preferences:</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Monthly Income:
+          <input
+            type="number"
+            value={income}
+            onChange={(e) => setIncome(e.target.value)}
+          />
+        </label>
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
-export default useUserInfo;
+export default InputInfo;
