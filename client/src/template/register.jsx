963f42864d4cc72d30a55345d283a18895b84be3
Matthew Tran <matthewtran0122@gmail.com>
2024-04-03 13:17:22 -0700

    css change on the register button
    just changed to a lighter shade of blue when button is disabled

--- client/src/template/register.jsx
+++ client/src/template/register.jsx
@@ -167,13 +167,17 @@ const Register = () => {
           
           <div className="mb-6">
             <span className="block w-full rounded-md shadow-sm">
-              <button
-                disabled={!validEmail || !validPwd || !validMatch ? true : false}
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
-              >
-                Create an Account
-              </button>
+            <button
+              disabled={!validEmail || !validPwd || !validMatch ? true : false}
+              type="submit"
+              className={`w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md ${
+                !validEmail || !validPwd || !validMatch
+                  ? 'bg-blue-300 text-white hover:bg-disabled focus:bg-disabled'
+                  : 'bg-blue-600 text-white hover:bg-blue-500 focus:bg-blue-500'
+              }`}
+            >
+              Create an Account
+            </button>
             </span>
           </div>
         </form>
