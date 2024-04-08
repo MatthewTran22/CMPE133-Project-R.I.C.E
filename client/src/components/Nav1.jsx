1e790011f80ead8de4a6a37d2d667e0cb6529ebf
Jason <hieu.hc04@gmail.com>
2024-04-08 13:04:08 -0700

    installed react-icon
    npm installed react-icons

--- client/src/components/Nav1.jsx
+++ client/src/components/Nav1.jsx
@@ -12,7 +12,7 @@ const Nav1 = () => {
         <div className="w-1/2"  onClick={() => { navigate("/") }}>
           <img src={Logo} style={{ width: '200px', height: 'auto' }} alt="Logo"  />
         </div>
-        <div className="w-1/2 flex text-white justify-end items-center mr-3 space-x-3">
+        <div className="w-1/2 flex t   sext-white justify-end items-center mr-3 space-x-3">
           <div className="text-white border-2 p-3 rounded-3xl h-1/2 flex items-center ease-in duration-300 hover:bg-white hover:text-black" onClick={() => { navigate("/reportpurchases") }}> 
             <p className="">Report Purchases</p>
           </div>
