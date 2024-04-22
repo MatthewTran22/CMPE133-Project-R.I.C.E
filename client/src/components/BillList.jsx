531f32233c148588acd0e0cd84c73590fa98dc32
Matthew Tran <matthewtran0122@gmail.com>
2024-04-21 21:55:27 -0700

    Revert "changed the button on bill list"
    This reverts commit 7f97d632b68cdfdebfb0c8c4ea2df53d2df78816.

--- client/src/components/BillList.jsx
+++ client/src/components/BillList.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-
 
 const BillList = () => {
-  const nav = useNavigate();
+  const [showForm, setShowForm] = useState(false);
 
   const handleToggleForm = () => {
-    
+    setShowForm(!showForm);
   };
 
   return (
@@ -15,14 +13,27 @@ const BillList = () => {
       <div className="absolute top-62% right-50% transform -translate-y-1/2" style={{ fontSize: '1rem' }}>
           <div className="h-24 ml-1 flex w-full items-center relative z-10">
             <div className="w-1/2 flex text-white justify-end items-center mr-3 space-x-3">
-              <div className="text-white border-2 p-3 rounded-3xl h-1/2 flex items-center ease-in duration-300 hover:bg-white hover:text-black" onClick={handleToggleForm} style={{ transform: 'translateX(345%) translateY(60%)' }}> 
+              <div className="text-white border-2 p-3 rounded-3xl h-1/2 flex items-center ease-in duration-300 hover:bg-white hover:text-black" onClick={handleToggleForm} style={{ transform: 'translateX(340%) translateY(60%)' }}> 
                 <p className="">Edit List</p>
               </div>
             </div>
           </div>
         </div>
         Bill List
-        <br/>
+        {showForm && (
+          <div className='mt-4'>
+            <form style={{ paddingLeft: '1rem' }} >
+              {/* Add form fields here */}
+              <div style={{ fontSize: '1rem' }}>
+                <input type='text' placeholder='Bill Name' className='p-2 border-2 border-black rounded ml-1rem' />
+                <input type='number' placeholder="Amount: $0.00" className='p-2 border-2 border-black rounded' />
+                <button type='submit' className='bg-black text-white p-2 rounded'>
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        )}<br/>
         <div style={{ fontSize: '2rem' }}>
             item1 <br/>
             item1 <br/>
