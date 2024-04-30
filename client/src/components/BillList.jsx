e2322514556c1e8349cdbfa5db832795f8840e6b
Matthew Tran <matthewtran0122@gmail.com>
2024-04-29 21:46:35 -0700

    tailwind changes for dashboard component
    changed to where the price is marked red for incomplete payment and green for completed payment

--- client/src/components/BillList.jsx
+++ client/src/components/BillList.jsx
@@ -31,12 +31,12 @@ const BillList = () => {
   };
 
   return (
-    <div className = 'w-96 min-h-96 h-200 rounded-3xl box-border p-4 border-4 whitespace-nowrap overflow-auto cursor-pointer bg-dark-blue' style={{transform: 'translateX(72%)'}}>
+    <div className = 'w-96 min-h-96 h-200 rounded-3xl box-border p-4 border-4 whitespace-nowrap overflow-auto  bg-dark-blue' style={{transform: 'translateX(72%)'}}>
       <div className='text-white text-5xl'>
       <div className="absolute top-62% right-50% transform -translate-y-1/2" style={{ fontSize: '1rem' }}>
           <div className="h-24 ml-1 flex w-full items-center relative z-10">
             <div className="w-1/2 flex text-white justify-end items-center mr-3 space-x-3">
-              <div className="text-white border-2 p-3 rounded-3xl h-1/2 flex items-center ease-in duration-300 hover:bg-white hover:text-black" onClick={handleToggleForm} style={{ transform: 'translateX(340%) translateY(60%)' }}> 
+              <div className="cursor-pointer text-white border-2 p-3 rounded-3xl h-1/2 flex items-center ease-in duration-300 hover:bg-white hover:text-black" onClick={handleToggleForm} style={{ transform: 'translateX(340%) translateY(60%)' }}> 
                 <p className="">Edit List</p>
               </div>
             </div>
@@ -60,10 +60,9 @@ const BillList = () => {
                       {description}
                     </div>
                     <div>
-                      ${bill.amount.toFixed(2)}
                     </div>
-                    <div>
-                      {bill.paid ? <FaCheck color="green" size="1.5rem"/> : <FaTimes color="red" size="1.5rem"/>}
+                    <div className={`${bill.paid === true ? 'text-green-500' : 'text-red-500'}`}>
+                     ${bill.amount.toFixed(2)}
                     </div>
                   </div>
                 </li>
