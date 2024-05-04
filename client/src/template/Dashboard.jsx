9b78fb92085ea38125e006b7009b16e1fd76a759
Matthew Tran <matthewtran0122@gmail.com>
2024-05-03 19:05:06 -0700

    Style Dashboard done
    styling on dashboard done now centered no matter what

--- client/src/template/Dashboard.jsx
+++ client/src/template/Dashboard.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import Chart from '../components/PieChart';
 import Bar from '../components/ThreeStepProgress';
 
-const Dashboard = () => { 
+const Dashboard = () => {
   const nav = useNavigate();
   const [info, setInfo] = useState([]);
   const [userName, setUserName] = useState('');
@@ -49,23 +49,19 @@ const Dashboard = () => {
         <div id="stars3"></div>
         <div id="title"></div>
         <Nav1 />
-        <div class="pl-[10rem] relative grid grid-cols-2 gap-[15rem] bg-transparent">
-          <div class=" relative bg-transparent p-4 grid place-items-end">
-
+        <div className="flex flex-col sm:flex-row gap-4 justify-center items-center w-full max-w-80rem mt-4">
+          <div className="relative bg-transparent p-4 flex flex-col items-center">
             <Totals Category="Current Total" otherTotals={info[0].total_remaining} /> <br/>
             <Chart data={info} />
           </div>
-          <div class="relative bg-transparent p-4">
+          <div className="relative bg-transparent p-4 flex flex-col items-center">
             <RecentTransactions /> <br/>
             <BillList /> <br/>
           </div>
         </div>
-        <div className="flex items-center justify-center">
-          <div className='justify-content-center'>
-            <Bar/>
-          </div>
-        </div><br/>
-        
+        <div className="flex flex-col sm:flex-row gap-4 justify-center items-center w-full max-w-80rem mt-4">
+          <Bar/>
+        </div>
       </div>
     </div>
   );
